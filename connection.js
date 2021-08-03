1df6ecb7f628d3999713cdb5d4c79be2aba78d38
cekiit23 <yogabenrc@gmail.com>
2021-08-03 21:24:16 +0700

    menambahkan file server.js dan connection.js

---
+++ connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+// membuat koneksi ke database
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database: 'restapi'
+
+})
+
+conn.connect( (error) => {
+    if(error) throw error;
+
+    console.log('mysql running')
+})
